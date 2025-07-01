906ee2568ea831f078f3eb2c08ab26fe701ba611
rosfandy <bagusrosfandy@gmail.com>
2025-07-01 10:46:35 +0700

    init commit

--- src/internal/database/models/squad.go
+++ src/internal/database/models/squad.go
@@ -2,17 +2,13 @@ package models
 
 import (
 	"time"
-
-	"gorm.io/datatypes"
 )
 
 type Squad struct {
-	ID               int64          `gorm:"primaryKey;autoIncrement" json:"id"`
-	Name             string         `gorm:"size:255;not null" json:"name"`
-	GeneralProjectID int64          `gorm:"column:general_project_id;not null" json:"general_project_id"`
-	Anggota          datatypes.JSON `gorm:"type:json" json:"anggota"`
-	CreatedAt        time.Time      `gorm:"column:created_at" json:"created_at,omitempty"`
-	UpdatedAt        time.Time      `gorm:"column:updated_at;autoUpdateTime" json:"updated_at"`
+	ID        int64     `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name      string    `gorm:"size:255;not null" json:"name"`
+	CreatedAt time.Time `gorm:"column:created_at" json:"created_at,omitempty"`
+	UpdatedAt time.Time `gorm:"column:updated_at;autoUpdateTime" json:"updated_at"`
 }
 
 func (Squad) TableName() string {
